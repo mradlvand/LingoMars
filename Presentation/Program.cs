58b3600fd3335fcf94c3b98d7bd823e7baaf32a9
mradlvand <rezaadlvand1370@gmail.com>
2024-08-02 12:35:11 +0330

    fix

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Presentation.Framework.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Presentation.Service;
 using System.Reflection;
 using Data.Context;
 using System.Text.Json.Serialization;
+using Presentation.Models.Aut;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +29,41 @@ builder.Services.AddScoped<IGrammerLogic, GrammerLogic>();
 builder.Services.AddScoped<ISpeakingLogic, SpeakingLogic>();
 builder.Services.AddScoped<IUserLogic, UserLogic>();
 
+#region "JWT Token For Authentication Login"    
+SiteKeys.Configure(builder.Configuration.GetSection("AppSettings"));
+var key = Encoding.ASCII.GetBytes(SiteKeys.Token);
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+});
+
+
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+ .AddJwtBearer(token =>
+ {
+     token.RequireHttpsMetadata = false;
+     token.SaveToken = true;
+     token.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuerSigningKey = true,
+         IssuerSigningKey = new SymmetricSecurityKey(key),
+         ValidateIssuer = true,
+         ValidIssuer = SiteKeys.WebSiteDomain,
+         ValidateAudience = true,
+         ValidAudience = SiteKeys.WebSiteDomain,
+         RequireExpirationTime = true,
+         ValidateLifetime = true,
+         ClockSkew = TimeSpan.Zero
+     };
+ });
+
+#endregion
+
 builder.Services.AddApiVersioning(options =>
 {
     options.DefaultApiVersion = new ApiVersion(1, 0);
@@ -43,7 +82,24 @@ app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
+#region "JWT Token For Authentication Login"    
+
+app.UseCookiePolicy();
+app.UseSession();
+app.Use(async (context, next) =>
+{
+    var JWToken = context.Session.GetString("JWToken");
+    if (!string.IsNullOrEmpty(JWToken))
+    {
+        context.Request.Headers.Add("Authorization", "Bearer " + JWToken);
+    }
+    await next();
+});
+app.UseAuthentication();
 app.UseAuthorization();
+
+
+#endregion
 
 app.MapControllers();
 
